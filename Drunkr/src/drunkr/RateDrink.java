d76729d647d90cb734af7a1549c0f1d5e8a0afc0
beese <kristinbeese@gmail.com>
2016-11-02 16:04:00 -0400

    Revert "fix grades and bac calc stuff"
    This reverts commit d8d7a7a6fb34a2c1ea3abce51b34c9d35297fb74.

--- Drunkr/src/drunkr/RateDrink.java
+++ Drunkr/src/drunkr/RateDrink.java
@@ -36,9 +36,8 @@ public class RateDrink extends JsonServlet {
 		double averageRating = (double)r.getProperty("averageRating");
 		long totalRatings = (long)r.getProperty("totalRatings");
 		int ratingSum = (int)(averageRating * totalRatings);
-		
 		//Normalize the alcoholPercentage to be out of 5
-		double alcoholRating= (double)r.getProperty("AlcoholContent") / 10.0f;
+		double alcoholRating= (((double)r.getProperty("AlcoholContent") * 100) / 10) / 2;
 		
 		totalRatings++;
 		ratingSum += rating;
